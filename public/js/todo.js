aba8407d3f0839df643f0e4cf3c73ab6a03b1e29
EasyHealth-cyber <bkhaidoume@gmail.com>
2026-02-15 14:38:06 +0000

    save data with local storage

--- public/js/todo.js
+++ public/js/todo.js
@@ -5,6 +5,18 @@ let notasks = document.getElementById("notasks");
 
 let tasks = [];
 
+function loadTasks() {
+  const savedTasks = JSON.parse(localStorage.getItem('tasks')); 
+  if (savedTasks) {
+    tasks = savedTasks;  
+  }
+  rendertasks();  
+}
+
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));  
+}
+
 function rendertasks(){
     taskslist.innerHTML = '';
 
@@ -16,22 +28,22 @@ function rendertasks(){
 
     tasks.forEach((task , index) => {
         const taskItem = document.createElement('li');
-        taskItem.classList.add('flex', 'justify-between', 'items-center', 'p-2', 'border-b');
+        taskItem.classList.add('flex', 'justify-between', 'items-center', 'p-4', 'border', 'border-gray-300', 'rounded-lg', 'mb-3', 'shadow-md');
 
         const taskText = document.createElement('span');
         taskText.textContent = task.text;
         if (task.completed) {
-      taskText.classList.add('line-through');
+      taskText.classList.add('line-through', 'text-red-500' );
     }
 
         const deleteButton = document.createElement('button');
          deleteButton.textContent = 'Delete';
-         deleteButton.classList.add('text-red-500', 'ml-4');
+         deleteButton.classList.add('text-red-500', 'ml-4', 'px-3', 'py-2', 'border', 'border-red-500', 'rounded-md', 'hover:bg-red-100');
          deleteButton.addEventListener('click', () => deletetask(index));
 
          const modifyButton = document.createElement('button');
     modifyButton.textContent = 'Modify';
-    modifyButton.classList.add('text-blue-500', 'ml-4');
+    modifyButton.classList.add('text-blue-500', 'ml-4', 'px-3', 'py-2', 'border', 'border-blue-500', 'rounded-md', 'hover:bg-blue-100', 'transition-colors');
     modifyButton.addEventListener('click', () => modifyTask(index));
 
          taskItem.appendChild(taskText);
@@ -49,12 +61,14 @@ function rendertasks(){
             const newTask = {text: taskText, completed: false};
             tasks.push(newTask);
             inputvalue.value = '';
+            saveTasks(); 
             rendertasks();
         };
     };
 
     function deletetask(index) {
         tasks.splice(index, 1);
+        saveTasks(); 
         rendertasks();
     };
 
@@ -62,11 +76,12 @@ function rendertasks(){
   const newTaskText = prompt('Edit task:', tasks[index].text);  
 
   if (newTaskText !== null && newTaskText.trim() !== '') {
-    tasks[index].text = newTaskText.trim();  // Update the task text in the array
-    rendertasks();  // Re-render tasks to reflect the changes
+    tasks[index].text = newTaskText.trim(); 
+    saveTasks();  
+    rendertasks();  
   }
 }
 
     addtaskbtn.addEventListener('click' , addtask);
 
-    rendertasks();
+    loadTasks();
